cf25df48912f747855be47c516d488eeb470f0f2
허강현 <hgh6128@gmail.com>
2025-05-04 23:34:29 +0900

    cors 설정

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 import authRoutes from './routes/auth.route.js';
 import mediaRoutes from './routes/media.route.js';
@@ -13,6 +14,13 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(
+    cors({
+        origin: 'http://localhost:5173',
+        credentials: true,
+    })
+);
 
 app.use(express.json());
 app.use(cookieParser());
